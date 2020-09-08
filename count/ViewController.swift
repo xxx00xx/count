643a0620a5533c2b01dffdf46878ae4c8aac3247
Shotaro Furuta <strfrt13xx46@gmail.com>
2020-09-09 01:04:55 +0900

    Initial Commit

---
+++ count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  count
+//
+//  Created by 古田翔太郎 on 2020/09/09.
+//  Copyright © 2020 古田翔太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
