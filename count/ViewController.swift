e25a9730568016c4b6d1591fbb6a858cee63a37f
Shotaro Furuta <strfrt13xx46@gmail.com>
2020-09-09 01:51:07 +0900

    カウントアプリを作成した．

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func mult(){
+        number = number * 2
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func div(){
+        number = number / 2
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func AllClear(){
+        number = 0
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func color(){
+        if(number >= 10){
+            label.textColor = UIColor.red
+        }else if(number <= -10){
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
 }
 
